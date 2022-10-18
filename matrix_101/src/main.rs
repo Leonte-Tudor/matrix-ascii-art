5e45d7f1072cab2061340e5148c9f951a131b396
Leon <tudor.flavian.leonte@gmail.com>
2022-10-18 20:32:45 +0300

    split everything into functions

--- matrix_101/src/main.rs
+++ matrix_101/src/main.rs
@@ -2,9 +2,89 @@ use std::cmp::min;
 use std::io;
 use colored::*;
 
+fn print_preamble(ind: usize) {
+    println!("\n{:ind$}{}{:ind$}{}{:ind$}{}{:ind$}{}",
+             " ", "====================================\n".green(),
+             " ", "Congratulations, you have fooled the\n",
+             " ", "     borrow checker once again\n",
+             " ", "====================================\n".green());
+}
+
+fn fill_diags(m: &mut Vec<i32>, c: usize, l: usize, d: usize) {
+    for i in 0..d {
+
+        // for j in ((i * c + c + i)..(i * c + l * (c - i) + i)).step_by(c){
+        //     m[j] = 1;
+        // }     // first try at filling matrix below main diagonal
+
+        for j in 1..l - i {
+            m[i * c + j * c + i] = 2;
+        }   // fill below main diagonal
+
+        for j in 1..i + 1 {
+            m[(i + 1) * c - j * c - i - 1] = 3;
+        }   // fill above anti diagonal
+    }
+}
+
+fn fill_west(m: &mut Vec<i32>, c: usize, c2: usize, l: usize, d: usize, flag_mode: bool) {
+    if flag_mode == true {
+        for i in 1..c2 {
+            for j in i..l - i {
+                m[i * c + (j - i) * c + i - 1] = 4; // fill west region
+            }
+        }
+    } else {
+        for i in 1..c2 {
+            for j in i..d - i {
+                m[i * c + (j - i) * c + i - 1] = 4; // fill west region
+            }
+        }
+    }
+}
+
+fn fill_indicators(m: &mut Vec<i32>, c: usize, c2: usize, l: usize, l2: usize, flag_mode: bool) {
+    for i in 1..l2 / 2 {
+        m[i * c + c2 - 1] = 6; // north indicator
+        m[c * (l - i - 1) + c2 - 1] = 7; // south indicator
+    }
+
+    for i in 1..c2 / 2 {
+        m[(c2 - 1) * c + i] = 8; // west indicator
+        m[c2 * c - i - 1] = 9; // east indicator
+    }
+}
+
+fn draw_matrix(m: &mut Vec<i32>, c: usize, c2: usize, l: usize, l2: usize, size: usize, ind: usize, flag_mode: bool, par: bool) {
+    for i in 0..size {
+        if i % c as usize == 0 {
+            print!("{:ind$}", "");
+        }
+
+        match m[i] {
+            0 => print!("{} ", "◀".to_string().yellow()),
+            1 => print!("{} ", "↘".white().dimmed()),
+            2 => print!("{} ", "▲".to_string().red()),
+            3 => print!("{} ", "▼".to_string().green()),
+            4 => print!("{} ", "▶".to_string().blue()),
+            5 => if par == false && i == (c) * (l2 - 1) + l2 - 1
+            { print!("{} ", "⤩".white().dimmed()); } else { print!("{} ", "↙".white().dimmed()); },
+            6 => print!("{} ", "▲".white().dimmed()),
+            7 => print!("{} ", "▼".white().dimmed()),
+            8 => print!("{} ", "◀".white().dimmed()),
+            9 => print!("{} ", "▶".white().dimmed()),
+            _ => print!("?!?"),
+        }
+
+        if (i + 1) % c as usize == 0 {
+            println!();
+        }
+    }
+    println!();
+}
+
 fn main() {
-
-
+    
     //println!("Insert column size (number of rows)")
     // let (mut l, mut c): (i32, i32);
     // let mut buff = String::new();
@@ -22,71 +102,38 @@ fn main() {
         println!("{}", x[i]);
     }*/
 
-    let (l, c) = (55, 55);
+    let (l, c) = (25, 25);
+    let size = (l * c) as usize;
+    let d = min(l, c);
     let indent: i32 = (c as i32 * 2 - 36) / 2;
     let mut ind: usize = if indent < 0 { 0 } else { indent as usize };
     let flag_mode = false;
-    let mut par:bool;
-    let c2 = if c % 2 == 0 { par = false; c/2 } else { par = true; c/2 + 1 };
-    let l2 = if l % 2 == 0 { par = false; l/2 } else { par = false; l/2 + 1 };
+    let mut par: bool;
+    let c2 = if c % 2 == 0 {
+        par = false;
+        c / 2
+    } else {
+        par = true;
+        c / 2 + 1
+    };
+    let l2 = if l % 2 == 0 {
+        par = false;
+        l / 2
+    } else {
+        par = false;
+        l / 2 + 1
+    };
     //let d2 = if d % 2 == 0 { par = false; d/2 } else { par = false; d/2 + 1 };
 
-    println!("\n{:ind$}{}{:ind$}{}{:ind$}{}{:ind$}{}",
-             " ", "====================================\n".green(),
-             " ", "Congratulations, you have fooled the\n",
-             " ", "     borrow checker once again\n",
-             " ", "====================================\n".green());
-
-
-    let size = (l * c) as usize;
-    let d = min(l, c);
+    print_preamble(ind);
 
     let mut m = vec![0; size];
 
+    fill_diags(&mut m, c, l, d);
 
-    for i in 0..d {
+    fill_west(&mut m, c, c2, l, d, flag_mode);
 
-        // for j in ((i * c + c + i)..(i * c + l * (c - i) + i)).step_by(c){
-        //     m[j] = 1;
-        // }     // first try at filling matrix below main diagonal
-
-        for j in 1..l - i {
-            m[i * c + j * c + i] = 2;
-        }   // fill below main diagonal
-
-        for j in 1..i + 1 {
-            m[(i + 1) * c - j * c - i - 1] = 3;
-        }   // fill above anti diagonal
-
-    }
-
-
-
-
-    if flag_mode == true {
-        for i in 1..c2 {
-            for j in i..l - i {
-                m[i * c + (j - i) * c + i - 1] = 4; // fill west region
-            }
-        }
-    }
-    else {
-        for i in 1..c2 {
-            for j in i..d - i {
-                m[i * c + (j - i) * c + i - 1] = 4; // fill west region
-            }
-        }
-    }
-
-    for i in 1..l2/2 {
-        m[i * c + c2 - 1] = 6; // north indicator
-        m[c * (l - i - 1) + c2 - 1] = 7; // south indicator
-    }
-
-    for i in 1..c2/2 {
-        m[(c2 - 1) * c + i] = 8; // west indicator
-        m[c2 * c - i - 1] = 9; // east indicator
-    }
+    fill_indicators(&mut m, c, c2, l, l2, flag_mode);
 
     for i in 0..d {
         m[i * c + i] = 1; // main diagonal
@@ -95,32 +142,6 @@ fn main() {
 
     ind = if ind == 0 { (-indent + 1) as usize } else { 1 };
 
+    draw_matrix(&mut m, c, c2, l, l2, size, ind, flag_mode, par);
 
-    for i in 0..size {
-
-        if i % c as usize == 0 {
-            print!("{:ind$}", "");
-        }
-
-        match m[i] {
-            0 => print!("{} ", "◀".to_string().yellow()),
-            1 => print!("{} ", "↘".white().dimmed()),
-            2 => print!("{} ", "▲".to_string().red()),
-            3 => print!("{} ", "▼".to_string().green()),
-            4 => print!("{} ", "▶".to_string().blue()),
-            5 => if par == false && i == (c) * (l2 - 1) + l2 - 1
-                 { print!("{} ", "⤩".white().dimmed()); }
-                 else { print!("{} ", "↙".white().dimmed()); },
-            6 => print!("{} ", "▲".white().dimmed()),
-            7 => print!("{} ", "▼".white().dimmed()),
-            8 => print!("{} ", "◀".white().dimmed()),
-            9 => print!("{} ", "▶".white().dimmed()),
-            _ => print!("?!?"),
-        }
-
-        if (i + 1) % c as usize == 0 {
-            println!();
-        }
-    }
-    println!();
 }
